953d2e489b6fb2cd89a4caccd507c9899a77fd04
Łukasz Kuk <lukasz010420@gmail.com>
2023-05-07 17:16:36 +0200

    add student

--- main.py
+++ main.py
@@ -1,46 +1,95 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QVBoxLayout, QLabel, QWidget, \
-    QGridLayout, QLineEdit, QPushButton
-from datetime import datetime
+import sqlite3
+
+from PyQt6.QtWidgets import QApplication, QMainWindow, QTableWidget, \
+    QTableWidgetItem, QDialog, QVBoxLayout, QLineEdit, QComboBox, QPushButton
+from PyQt6.QtGui import QAction
 
 
-class AgeCalculator(QWidget):
+class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        grid = QGridLayout()
-        self.setWindowTitle("Age Calculator")
+        self.setWindowTitle("Student Management System")
 
-        name_label = QLabel("Name:")
-        self.name_line_edit = QLineEdit()
+        file_menu_item = self.menuBar().addMenu("&File")
+        help_menu_item = self.menuBar().addMenu("&Help")
 
-        date_birth_label = QLabel("Date of Birth MM/DD/YYYY:")
-        self.date_birth_line_edit = QLineEdit()
+        add_student_action = QAction("Add Student", self)
+        add_student_action.triggered.connect(self.insert)
+        file_menu_item.addAction(add_student_action)
 
-        calculate_button = QPushButton("Calculate Age")
-        calculate_button.clicked.connect(self.calculate_age)
-        self.output_label = QLabel("")
+        about_action = QAction("About", self)
+        help_menu_item.addAction(about_action)
 
-        grid.addWidget(name_label, 0, 0)
-        grid.addWidget(self.name_line_edit, 0, 1)
-        grid.addWidget(date_birth_label, 1, 0)
-        grid.addWidget(self.date_birth_line_edit, 1, 1)
-        grid.addWidget(calculate_button, 2, 0, 1, 2)
-        grid.addWidget(self.output_label, 3, 0, 1, 2)
+        self.table = QTableWidget()
+        self.table.setColumnCount(4)
+        self.table.setHorizontalHeaderLabels(
+            ("Id", "Name", "Course", "Mobile"))
+        self.table.verticalHeader().setVisible(False)
+        self.setCentralWidget(self.table)
+        self.load_data()
 
-        self.setLayout(grid)
+    def load_data(self):
+        connection = sqlite3.connect("database.db")
+        result = connection.execute("select * from students")
+        self.table.setRowCount(0)
+        for row_number, row_data in enumerate(result):
+            self.table.insertRow(row_number)
+            for column_number, data in enumerate(row_data):
+                self.table.setItem(row_number, column_number,
+                                   QTableWidgetItem(str(data)))
+        connection.close()
 
-    def calculate_age(self):
-        current_year = datetime.now().year
-        date_of_birth = self.date_birth_line_edit.text()
-        year_of_birth = datetime.strptime(date_of_birth,
-                                          "%m/%d/%Y").date().year
-        age = current_year - year_of_birth
-        self.output_label.setText(
-            f"{self.name_line_edit.text()} is {age} years old")
-        return
+    def insert(self):
+        dialog = InsertDialog()
+        dialog.exec()
+
+
+class InsertDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Student Management System")
+
+        self.setFixedWidth(300)
+        self.setFixedHeight(300)
+
+        layout = QVBoxLayout()
+
+        self.student_name = QLineEdit()
+        self.student_name.setPlaceholderText("Name")
+        layout.addWidget(self.student_name)
+
+        self.course_name = QComboBox()
+        courses = ["Biology", "Math", "Astroomy", "Physics"]
+        self.course_name.addItems(courses)
+        layout.addWidget(self.course_name)
+
+        self.mobile = QLineEdit()
+        self.mobile.setPlaceholderText("Mobile")
+        layout.addWidget(self.mobile)
+
+        button = QPushButton("Register")
+        button.clicked.connect(self.add_student)
+        layout.addWidget(button)
+
+        self.setLayout(layout)
+
+    def add_student(self):
+        connection = sqlite3.connect("database.db")
+        cursor = connection.cursor()
+        name = self.student_name.text()
+        course = self.course_name.itemText(self.course_name.currentIndex())
+        mobile = self.mobile.text()
+        cursor.execute(
+            "insert into students (name, course, mobile) values (?, ?, ?)",
+            (name, course, mobile))
+        connection.commit()
+        cursor.close()
+        connection.close()
+        main.load_data()
 
 
 app = QApplication(sys.argv)
-age_calculator = AgeCalculator()
-age_calculator.show()
+main = MainWindow()
+main.show()
 sys.exit(app.exec())
